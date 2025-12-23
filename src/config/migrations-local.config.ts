46996dce459af6cf17a17b1de405fd741d282be8
ChristianPenev <krunkiemonkey@gmail.com>
2025-12-23 23:32:37 +0200

    Created player entity

--- src/config/migrations-local.config.ts
+++ src/config/migrations-local.config.ts
@@ -4,8 +4,8 @@ export default new DataSource({
   type: 'postgres',
   host: 'aws-1-eu-central-1.pooler.supabase.com',
   port: 5432,
-  username: 'postgres.syaspxqmzugybgoftbhg',
-  password: 'Amatr@123!',
+  username: 'postgres.qlhpnyxosnoaqqjabuar',
+  password: 'SmgCup!123',
   database: 'postgres', // Don't forget to put the name of your init.sql database here
   migrations: ['src/db/migrations/*{.ts,.js}'],
   entities: ['src/**/*.entity{.ts,.js}'],
--- src/db/migrations/1766525518941-created-player-entity.ts
+++ src/db/migrations/1766525518941-created-player-entity.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreatedPlayerEntity1766525329292 implements MigrationInterface {
-    name = 'CreatedPlayerEntity1766525329292'
+export class CreatedPlayerEntity1766525518941 implements MigrationInterface {
+    name = 'CreatedPlayerEntity1766525518941'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "player" ("id" uuid NOT NULL, "first_name" text NOT NULL, "last_name" text NOT NULL, "year_of_birth" integer NOT NULL, "height" integer NOT NULL, "image_url" text, CONSTRAINT "PK_65edadc946a7faf4b638d5e8885" PRIMARY KEY ("id"))`);
