a06d370cac8635892d4af0f8e235c6fefcf617f0
Tonkata-hub <tonyyankOV07@gmail.com>
2026-01-06 17:09:49 +0200

    remove/edit players and teams

--- src/player/player.resolver.ts
+++ src/player/player.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Player } from './entities/player.entity';
 import { PlayerService } from './player.service';
 import { CreatePlayerDto } from './dto/create-player.dto';
+import { UpdatePlayerDto } from './dto/update-player.dto';
 
 @Resolver(() => Player)
 export class PlayerResolver {
@@ -27,5 +28,18 @@ export class PlayerResolver {
     @Args('createPlayerDto', { type: () => CreatePlayerDto }) createPlayerDto: CreatePlayerDto,
   ): Promise<Player> {
     return await this.playerService.createPlayer(createPlayerDto);
+  }
+
+  @Mutation(() => Player, { name: 'updatePlayer' })
+  async updatePlayer(
+    @Args('id', { type: () => String }) id: string,
+    @Args('updatePlayerDto', { type: () => UpdatePlayerDto }) updatePlayerDto: UpdatePlayerDto,
+  ): Promise<Player> {
+    return await this.playerService.updatePlayer(id, updatePlayerDto);
+  }
+
+  @Mutation(() => Player, { name: 'deletePlayer' })
+  async deletePlayer(@Args('id', { type: () => String }) id: string): Promise<Player> {
+    return await this.playerService.deletePlayer(id);
   }
 }
---
+++ src/team/dto/update-team.dto.ts
@@ -0,0 +1,14 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+
+@InputType()
+export class UpdateTeamDto {
+  @Field({ nullable: true })
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(255)
+  @IsString()
+  name?: string;
+}
+
+
