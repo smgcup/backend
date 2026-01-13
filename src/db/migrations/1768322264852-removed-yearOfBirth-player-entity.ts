1003eebc4c953a92852b0328456dfe0afd27035b
ChristianPenev <krunkiemonkey@gmail.com>
2026-01-13 18:39:33 +0200

    Created migration for removing the yearOfBirth

---
+++ src/db/migrations/1768322264852-removed-yearOfBirth-player-entity.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class RemovedYearOfBirthPlayerEntity1768322264852 implements MigrationInterface {
+    name = 'RemovedYearOfBirthPlayerEntity1768322264852'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "player" DROP COLUMN "year_of_birth"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "player" ADD "year_of_birth" integer NOT NULL`);
+    }
+
+}
