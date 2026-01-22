225f10ec61bc606ad33605aad24f6fdcaed2139c
ChristianPenev <krunkiemonkey@gmail.com>
2026-01-23 01:53:01 +0200

    Added constants for match values validation

--- src/match/dto/create-match.dto.ts
+++ src/match/dto/create-match.dto.ts
@@ -2,6 +2,7 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsDate, IsEnum, IsInt, IsNotEmpty, IsString, IsUUID, Max, Min } from 'class-validator';
 import { MatchStatus } from '../enums/match-status.enum';
 import { MATCH_TRANSLATION_CODES } from '../../exception/translation-codes/match.translation-codes';
+import { MATCH_ROUND_VALUES } from '../match.constants';
 
 @InputType()
 export class CreateMatchDto {
@@ -30,7 +31,7 @@ export class CreateMatchDto {
   @Field(() => Int)
   @IsNotEmpty()
   @IsInt()
-  @Min(1, { message: MATCH_TRANSLATION_CODES.matchRoundTooLow })
-  @Max(4, { message: MATCH_TRANSLATION_CODES.matchRoundTooHigh })
+  @Min(MATCH_ROUND_VALUES.MIN, { message: MATCH_TRANSLATION_CODES.matchRoundTooLow })
+  @Max(MATCH_ROUND_VALUES.MAX, { message: MATCH_TRANSLATION_CODES.matchRoundTooHigh })
   round: number;
 }
--- src/match/dto/update-match.dto.ts
+++ src/match/dto/update-match.dto.ts
@@ -2,6 +2,7 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsDate, IsEnum, IsInt, IsOptional, IsString, IsUUID, Max, Min } from 'class-validator';
 import { MatchStatus } from '../enums/match-status.enum';
 import { MATCH_TRANSLATION_CODES } from '../../exception/translation-codes/match.translation-codes';
+import { MATCH_ROUND_VALUES } from '../match.constants';
 
 @InputType()
 export class UpdateMatchDto {
@@ -42,7 +43,7 @@ export class UpdateMatchDto {
   @Field(() => Int, { nullable: true })
   @IsOptional()
   @IsInt()
-  @Min(1, { message: MATCH_TRANSLATION_CODES.matchRoundTooLow })
-  @Max(4, { message: MATCH_TRANSLATION_CODES.matchRoundTooHigh })
+  @Min(MATCH_ROUND_VALUES.MIN, { message: MATCH_TRANSLATION_CODES.matchRoundTooLow })
+  @Max(MATCH_ROUND_VALUES.MAX, { message: MATCH_TRANSLATION_CODES.matchRoundTooHigh })
   round?: number;
 }
