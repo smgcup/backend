d34c0dcd827bf9eb350d42fcad57142fb1d90f30
Tonkata-hub <tonyyankOV07@gmail.com>
2026-01-30 18:02:30 +0200

    malinovsky changes

--- src/db/migrations/1768318312466-added-dateOfBirth-to-player-entity.ts
+++ src/db/migrations/1768318312466-added-dateOfBirth-to-player-entity.ts
@@ -1,16 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddedDateOfBirthToPlayerEntity1768318312466 implements MigrationInterface {
-    name = 'AddedDateOfBirthToPlayerEntity1768318312466'
+  name = 'AddedDateOfBirthToPlayerEntity1768318312466';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "match_event" DROP COLUMN "created_at"`);
-        await queryRunner.query(`ALTER TABLE "match_event" ADD "created_at" TIMESTAMP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "match_event" DROP COLUMN "created_at"`);
+    await queryRunner.query(`ALTER TABLE "match_event" ADD "created_at" TIMESTAMP NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "match_event" DROP COLUMN "created_at"`);
-        await queryRunner.query(`ALTER TABLE "match_event" ADD "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "match_event" DROP COLUMN "created_at"`);
+    await queryRunner.query(
+      `ALTER TABLE "match_event" ADD "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
+    );
+  }
 }
