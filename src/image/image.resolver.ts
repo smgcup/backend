0f63d927f88be44e18011e6d4659016668e3c541
ChristianPenev <krunkiemonkey@gmail.com>
2026-02-07 19:05:37 +0200

    Added admin auth guard

--- src/image/image.resolver.ts
+++ src/image/image.resolver.ts
@@ -2,11 +2,14 @@ import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { ImageService } from './image.service';
 import { UploadFileDto } from './dto/upload-file.dto';
 import { UploadResponse } from './types/upload-response.type';
+import { AdminAuthGuard } from '../admin/auth/guards/admin-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver()
 export class ImageResolver {
   constructor(private readonly imageService: ImageService) {}
 
+  @UseGuards(AdminAuthGuard)
   @Mutation(() => UploadResponse, { name: 'uploadFile' })
   async uploadFile(@Args('uploadFileDto') uploadFileDto: UploadFileDto): Promise<UploadResponse> {
     return await this.imageService.uploadFile(uploadFileDto);
